bcf630294e1480736035ef232bad4b941be0299a
Nahom Tsegaye <nahomtsegaye6@gmail.com>
2026-02-27 12:53:05 -0500

    Started editing imu code

--- src/audio_esp32/src/main.c
+++ src/audio_esp32/src/main.c
@@ -26,7 +26,7 @@ i2c_slave_dev_handle_t i2c_slave_handle = NULL;
 
 void app_main(void)
 {
-    /* Initialize NVS — it is used to store PHY calibration data for bluetooth */
+    /* Initialize NVS — it is used to store PHY calibration data and data for Bluetooth */
     esp_err_t ret = nvs_flash_init();
     if  (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
--- src/imu_esp32/src/main.c
+++ src/imu_esp32/src/main.c
@@ -1,13 +1,14 @@
 #include "ble_interface.h"
 #include "mpu6050.h"
+
 const float ALPHA = 0.98f; // Complementary Filter: 98% Gyro, 2% Accel
-void
-app_main(void)
+//
+void app_main(void)
 {
     uint8_t buffer[14];   
     I2C_Queue = xQueueCreate(10, sizeof(buffer)); // create I2C data buffer queue
 
-    // 1. Setup I2C
+    // Setup I2C
     i2c_master_bus_handle_t bus_handle;
     i2c_master_dev_handle_t dev_handle;
     i2c_master_bus_config_t bus_config = {
@@ -27,18 +28,11 @@ app_main(void)
     };
     ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_config, &dev_handle));
 
-    // 2. Initialize Sensor
+    // Initialize Sensor
     InitMPU6050(dev_handle);
     
-    // 3. Calibrate (Keep sensor still!)
+    // Calibrate (Keep sensor still!)
     CalibrateGyro(dev_handle);
-
-    // 4. Variables for Filter
-    float filtered_pitch = 0.0f;
-    float filtered_roll = 0.0f;
-    int64_t last_time = esp_timer_get_time(); // Time in microseconds
-
-    int print_counter = 0;
 
     /* Initialize NVS — it is used to store PHY calibration data */
     esp_err_t ret = nvs_flash_init();
@@ -69,21 +63,6 @@ app_main(void)
     ble_hs_cfg.store_status_cb = ble_store_util_status_rr;
 
     ble_hs_cfg.sm_io_cap = CONFIG_EXAMPLE_IO_TYPE;
-#ifdef CONFIG_EXAMPLE_BONDING
-    ble_hs_cfg.sm_bonding = 1;
-#endif
-#ifdef CONFIG_EXAMPLE_MITM
-    ble_hs_cfg.sm_mitm = 1;
-#endif
-#ifdef CONFIG_EXAMPLE_USE_SC
-    ble_hs_cfg.sm_sc = 1;
-#else
-    ble_hs_cfg.sm_sc = 0;
-#endif
-#ifdef CONFIG_EXAMPLE_BONDING
-    ble_hs_cfg.sm_our_key_dist = 1;
-    ble_hs_cfg.sm_their_key_dist = 1;
-#endif
 
 #if MYNEWT_VAL(BLE_GATTS)
     int rc;
@@ -101,36 +80,10 @@ app_main(void)
 
     nimble_port_freertos_init(ble_spp_server_host_task);
     
-    // 5. Main Loopaccel_x
+    /* Read in a packet from I2C */
     while(1) {
-        // // A. Calculate Delta Time (dt) accurately
-        // int64_t current_time = esp_timer_get_time();
-        // float dt = (current_time - last_time) / 1000000.0f; // Convert us to seconds
-        // last_time = current_time;
-
-        
         ReadSensorData(dev_handle, buffer);
         xQueueSend(I2C_Queue,buffer,0); // send data buffer to the queue
-
-        // C. COMPLEMENTARY FILTER (The Fix for Drift)
-        // Formula: Angle = 0.98 * (Angle + Gyro_Rate * dt) + 0.02 * Accel_Angle
-        
-        // filtered_pitch = ALPHA * (filtered_pitch + data.gyro_y_dps * dt) + (1.0f - ALPHA) * data.pitch_accel;
-        // filtered_roll  = ALPHA * (filtered_roll  + data.gyro_x_dps * dt) + (1.0f - ALPHA) * data.roll_accel;
-
-        // D. Mapping (Corrected Range)
-        // Previous code mapped -45..45 to 0..180. This caused "dead zones" past 45 degrees.
-        // New map: -90..90 to 0..180 (Full 180 degree motion)
-        // float output_pitch = Map(filtered_pitch, -90.0f, 90.0f, 0.0f, 180.0f);
-        // float output_roll  = Map(filtered_roll,  -90.0f, 90.0f, 0.0f, 180.0f);
-
-        // // Print every 100ms
-        // if (print_counter++ >= 10) { 
-        //     // Note: filtered_pitch/roll are the REAL angles. output_pitch/roll are mapped 0-180.
-        //     ESP_LOGI(TAG, "Pitch: %.2f | Roll: %.2f", output_pitch, output_roll);
-        //     print_counter = 0;
-        // }
-
         vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
